1785e7b6a0beadf73aa9c4bb6adbd26302672090
luengwaiban <407261380@qq.com>
2019-06-10 11:13:33 +0800

    update setup

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='instagram-python-scraper',
-    version='1.0',
+    version='1.0.1',
     description=(
         'A instagram scraper wrote in python. Various actions supported.Enjoy it!'
     ),
